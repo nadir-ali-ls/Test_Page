bd06888549a0239a88a77cb065861020fd56e3ac
nadir-ali-ls <ghamehussain554@gmail.com>
2024-09-11 23:35:19 +0500

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,18 @@
+let menu = document.querySelector("header .menu .icon");
+let nav = document.querySelector(".navigation");
+
+let bar = '<ion-icon name="reorder-three-outline"></ion-icon>';
+let menBar = 'three';
+menu.addEventListener("click", () => {
+    if (menBar == 'three') {
+        menu.innerHTML = '<ion-icon name="close-outline"></ion-icon>';
+        menBar = 'close';
+        nav.classList.add("display");
+    } else {
+        menu.innerHTML = '<ion-icon name="reorder-three-outline"></ion-icon>';
+        menBar = 'three';
+        nav.classList.remove("display");
+    }
+    
+})
 
